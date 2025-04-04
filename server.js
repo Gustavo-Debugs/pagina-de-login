981eb95be22c7f5333b7717dec62e0139a52bfa0
Gustavo de Oliveira Silva <gustavooliv196@gmail.com>
2025-04-03 21:43:00 -0300

    back end quase pronto
    back end quase pronto so faltando arrumar o delete e get

--- server.js
+++ server.js
@@ -1,16 +1,78 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json()); // Middleware para analisar o corpo da requisição como JSON
 
-app.get('/usuarios', (request, response) =>{
-  res.send('ok, deu bom')
-})
+app.post("/usuarios", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    },
+  });
 
-app.listen(3000)
+  res.status(201).json(req.body);
+});
 
+app.get("/usuarios", async (req, res) => {
+  let user = []
+
+  if(req.query){
+    users = await prisma.user.findMany({
+      where: {
+        email: req.query.email,
+        name: req.query.name,
+        password: req.query.password
+      }
+    })
+  } else {
+    users = await prisma.user.findMany();
+  }
+  
+   
+  res.status(200).json(users);
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    },
+  });
+
+  res.status(201).json(req.body);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json({ message: 'Usuario deletado com sucesso' });
+});
+
+app.listen(3000);
 
 /*
-  1) tipo de rota / metodo HTTP
-  2) ENDEREÇO DA ROTA
+
+ - criar um usuario
+ - listar usuarios
+ - editar usuario
+ - deletar usuario
+ 
+  
+ - usuario:  gustavooliv196
+ - Senha: *14882010Gus
+
 */
